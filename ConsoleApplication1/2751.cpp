0ee56ecea20e6c76dc4d7f0b5e5f8c5e6023eef6
hhlee <gushak@naver.com>
2023-06-07 16:00:31 +0900

    [Sort 2751]

--- ConsoleApplication1/2751.cpp
+++ ConsoleApplication1/2751.cpp
@@ -1,7 +1,57 @@
 #include "pch.h"
 
-bool dp[2'000'003];
-int answer[2'000'003];
+void merge(int * A, int begin, int middle, int end, int* sortedarray)
+{
+	int sortedInd = begin;
+	int i = begin;
+	int j = middle + 1;
+
+	while (i <= middle && j <= end)
+	{
+		if (A[i] < A[j])
+		{
+			sortedarray[sortedInd] = A[i++];
+		}
+		else
+		{
+			sortedarray[sortedInd] = A[j++];
+		}
+		++sortedInd;
+	}
+
+	if (i > middle)
+	{
+		while (j <= end)
+		{
+			sortedarray[sortedInd] = A[j++];
+			sortedInd++;
+		}
+	}
+
+	if (j > end)
+	{
+		while (i <= middle)
+		{
+			sortedarray[sortedInd] = A[i++];
+			sortedInd++;
+		}
+	}
+
+	for (int k = begin; k <= end; ++k) A[k] = sortedarray[k];
+}
+
+void mergeSort(int* A, int begin, int end, int* array)
+{
+	if (begin < end)
+	{
+		int middle = (begin + end) / 2;
+		mergeSort(A, begin, middle, array);
+		mergeSort(A, middle + 1, end, array);
+
+		merge(A, begin, middle, end, array);
+	}
+}
+
 
 int main()
 {
@@ -9,34 +59,18 @@ int main()
 
 	cin >> n;
 
+	int* A = new int[n];
+	int* sortedarray = new int[n];
 	for (int i = 0; i < n; ++i)
 	{
-		int number = 0;
-		int index = 0;
-		cin >> number;
-
-		if (number > 0)
-		{
-			index = number + 1'000'001;
-		}
-		else
-		{
-			index = abs(number);
-		}
-
-		if (!dp[index])
-		{
-			dp[index] = true;
-
-			answer[index] = number;
-		}
+		cin >> A[i];
 	}
 
-	for (int i = 0; i < 2'000'001; ++i)
+	mergeSort(A, 0, n - 1, sortedarray);
+
+	for (int i = 0; i < n; ++i)
 	{
-		if(dp[i])
-			cout << answer[i] << endl;
+		cout << A[i] << endl;
 	}
-
 	return 0;
 }
