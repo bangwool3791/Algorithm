ea020b26ccba47a56cbd4dfed13c032ee8c4e290
hhlee <gushak@naver.com>
2023-06-11 03:28:41 +0900

    프로그래머스 두원 사이의 정수 쌍

---
+++ ConsoleApplication1/두 원사이의 정수쌍.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+long long solution(int r1, int r2) {
+    long long answer = 0;
+
+    for (int i = 1; i < r2; ++i)
+    {
+        int BR = 0;
+        int SR = 0;
+
+        BR = (int)floor(sqrt(pow(r2, 2) - pow(i, 2)));
+
+        if (i < r1)
+        {
+            SR = (int)ceil(sqrt(pow(r1, 2) - pow(i, 2)));
+        }
+        else
+        {
+            SR = 1;
+        }
+
+        answer += BR - SR + 1;
+    }
+    return 4 * (answer + r2 - r1 + 1);
+}
